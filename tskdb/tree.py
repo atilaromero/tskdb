3583351ea1f3abbce724f72042ea6b35669f8629
atila.alr <atila.alr@z820-alr.localdomain>
2014-07-25 16:05:02 -0300

    delete unused methods

--- tskdb/tree.py
+++ tskdb/tree.py
@@ -123,50 +123,6 @@ class Tree(dict):
             return super(Tree,self).__getitem__(key)
     __getitem__.__doc__ = __setitem__.__doc__
 
-class DefaultDict(Tree,collections.defaultdict):
-    def __init__(self,*args,**kwargs):
-        super(DefaultDict,self).__init__(*args,**kwargs)
-        self.default_factory = self.__class__
-    def __repr__(self):
-        return str(dict(self))
-
-class Compare(Tree):
-    def strdates(self, *args):
-        s = [''] * len(args)
-        if hasattr(self, 'dates'):
-            s = self.dates
-            s = ['{0}'.format(s[x]) for x in args]
-        return ' '.join(['{0:12}'.format(x) for x in s])
-    def strmd5s(self, *args):
-        s = [''] * len(args)
-        if hasattr(self, 'md5s'):
-            s = self.md5s
-            s = [s[x] for x in args]
-        return ' '.join([x[:4].ljust(4) for x in  s])
-    def getcompare(self, field, x, y, comparefunc):
-        def check(v):
-            if not hasattr(v, field):
-                return True #ignore, is not a leaf
-            vf = getattr(v, field)
-            if vf and vf[x] and vf[y]: #otherwise, ignore
-                if not comparefunc(vf[x], vf[y]):
-                    return False #only fails if comparison is possible
-            return True
-        return self.checkdeep(check)
-    def getainb(self, x, y):
-        def check(v):
-            if not hasattr(v, 'dates'):
-                return True #ignore, is not a leaf
-            vf = getattr(v, 'dates')
-            return  (vf[x] and vf[y]) or not vf[x]
-        return self.checkdeep(check)
-    def getmd5equal(self, x, y):
-        return self.getcompare('md5s', x, y, lambda a, b: a == b or 'd41d8cd98f00b204e9800998ecf8427e' in (a, b))
-    def getdateequal(self, x, y):
-        return self.getcompare('dates', x, y, lambda a, b: a == b)
-    def getdatebigger(self, x, y):
-        return self.getcompare('dates', x, y, lambda a, b: a > b)
-
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
