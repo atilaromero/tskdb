5d45a6e9efbc19cea98bea15f56bb588f13cbff8
atila.alr <atila.alr@z820-alr.localdomain>
2014-07-25 16:34:36 -0300

    class improvement

--- tskdb/tsktree.py
+++ tskdb/tsktree.py
@@ -3,18 +3,15 @@ from db import loaddbsqlite
 from tree import Tree
 
 class TskTree(Tree):
-    def __init__(self):
+    def __init__(self, dbpath=None):
         super(Tree,self).__init__()
         self.files = []
-
-def main(path):
-    meta,session = loaddbsqlite(path)
-    tsk_files=meta.tbcls.tsk_files
-    tree = TskTree()
-    for rec in session.query(tsk_files).all():
-        if rec.parent_path:
-            tree[rec.parent_path.strip('/').split('/')].files.append(rec)
-        else:
-            tree[rec.parent_path].files.append(rec)
-    return tree
+        if dbpath != None:
+            meta, session = loaddbsqlite(dbpath)
+            tsk_files=meta.tbcls.tsk_files
+            for rec in session.query(tsk_files).all():
+                if rec.parent_path:
+                    self[rec.parent_path.strip('/').split('/')].files.append(rec)
+                else: # no parent?
+                    self[rec.parent_path].files.append(rec)
         
