8ec51fdbce40e1cbd2295d4f2481e1d49aa9ff6b
atila.alr <atila.alr@z820-alr.localdomain>
2014-07-25 16:34:16 -0300

    remove unused code

--- tskdb/tsktree.py
+++ tskdb/tsktree.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
-import os
-import datetime
-import collections
 from db import loaddbsqlite
 from tree import Tree
-
-def findfiles(meta,session,path):
-    tsk_files=meta.tbcls.tsk_files
-    for f in session.query(tsk_files)\
-                    .filter(tsk_files.parent_path.startswith(path))\
-                    .filter(tsk_files.meta_type==1)\
-                    .filter(tsk_files.size>0)\
-                    .all():
-        yield f.mtime,os.path.join(f.parent_path.split(path,1)[1],f.name)
 
 class TskTree(Tree):
     def __init__(self):
