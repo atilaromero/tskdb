30f3e18dce04436dd9e8ec9a58ea38c1c083612a
atila.alr <atila.alr@z820-alr.localdomain>
2014-08-06 09:58:56 -0300

    use thread to read file

--- tskdb/strings.py
+++ tskdb/strings.py
@@ -41,56 +41,69 @@ regsexpr['utf_32_be']='\x00\x00\x00[%s]'%(''.join([c.encode('latin1') for c in c
 regsexpr['utf_16_le']='[%s]\x00'%(''.join([c.encode('latin1') for c in chars]))
 regsexpr['utf_32_le']='[%s]\x00\x00\x00'%(''.join([c.encode('latin1') for c in chars]))
     
+import multiprocessing
+import multiprocessing.dummy
+
+def _strings_producer(srcpath, queue, blksize):
+    with open(srcpath) as f:
+        while True:
+            chunk = f.read(blksize)
+            queue.put(chunk, True)
+            if len(chunk) == 0:
+                break
+
 def strings(encoding,srcpath,minimum=5,maximum=20):
-    blksize=1024**2 #1MB
+    blksize = 1024**2 #1MB
+    queue = multiprocessing.Queue(40) #40MB
     recall=21*5
     regs={}
     for x in encodings:
         regs[x]=re.compile('(?:%s){%s,%s}'%(regsexpr[x],minimum,maximum+1))
-    with open(srcpath) as f:
-        bl1=bl2=''
-        t1=t2=datetime.datetime.now()
-        pos={}
-        offset=0
-        while True:
-            if len(bl1)>recall:
-                skip=len(bl1)-recall
-            else:
-                skip=len(bl1)
-            t2=datetime.datetime.now()
-            deltat=t2-t1
-            speed=(skip/1024**2)/deltat.total_seconds()
-            sys.stderr.write('    %4.2fMB\t%1.2fMB/s\r'%(offset/1024.0**2,speed))
-            t1=t2
-            offset+=skip
-            bl1=bl1[skip:]+bl2
-            bl2=f.read(blksize)
-            if len(bl2)==0:
-                for k in sorted(pos.keys()):
-                    yield k,pos.pop(k)
-                break
-            blt=bl1+bl2
-            for x in [encoding,]:
-            #for x in encodings:
-                for found in regs[x].finditer(blt):
-                    position=found.start()+offset
-                    try:
-                        pass
-                        text=found.group().decode(x)
-                        if len(text)>maximum:
-                            continue
-                    except:
-                        print 'blt',[blt,]
-                        print [found.string,]
-                        pdb.set_trace()
-                    if pos.has_key(position):
-                        if len(text)>len(pos[position]):
-                            pos[position]=text
-                    else:
-                        pos[position]=text
+    producer = multiprocessing.dummy.Process(target=_strings_producer, args=(srcpath, queue, blksize))
+    producer.daemon = True
+    producer.start()
+    bl1=bl2=''
+    t1=t2=datetime.datetime.now()
+    pos={}
+    offset=0
+    while True:
+        if len(bl1)>recall:
+            skip=len(bl1)-recall
+        else:
+            skip=len(bl1)
+        t2=datetime.datetime.now()
+        deltat=t2-t1
+        speed=(skip/1024**2)/deltat.total_seconds()
+        sys.stderr.write('    %4.2fMB\t%1.2fMB/s\r'%(offset/1024.0**2,speed))
+        t1=t2
+        offset+=skip
+        bl1=bl1[skip:]+bl2
+        bl2=queue.get(True)
+        if len(bl2)==0: # EOF
             for k in sorted(pos.keys()):
-                if k<offset:
-                    yield k,pos.pop(k)
+                yield k,pos.pop(k)
+            break
+        blt=bl1+bl2
+        for x in [encoding,]:
+            for found in regs[x].finditer(blt):
+                position=found.start()+offset
+                try:
+                    pass
+                    text=found.group().decode(x)
+                    if len(text)>maximum:
+                        continue
+                except:
+                    print 'blt',[blt,]
+                    print [found.string,]
+                    pdb.set_trace()
+                if pos.has_key(position):
+                    if len(text)>len(pos[position]):
+                        pos[position]=text
+                else:
+                    pos[position]=text
+        for k in sorted(pos.keys()):
+            if k<offset:
+                yield k,pos.pop(k)
 
 def printfound(position,text,files):
     line = u'\t'.join([unicode(position),unicode(text)]+files)
